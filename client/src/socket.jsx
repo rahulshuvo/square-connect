1ffc8400cb466239bbbbb94feb719bc945b86242
rahulshuvo <rahulshuvo5@gmail.com>
2024-11-22 18:24:20 +0100

    URl changed

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client"; // import connection function
 
-const socket = io(import.meta.env.VITE_SOCKET_URL || 'localhost:8080'); // initialize websocket connection
+const socket = io(import.meta.env.VITE_SOCKET_URL || 'https://square-connect.onrender.com/'); // initialize websocket connection
 
 export default socket;
